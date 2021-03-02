42881c99e41dfcf9cd07464f5290670f85aa3220
Cmfauvel <Cm.fauvel@gmail.com>
2021-03-02 12:15:20 +0100

    debug json

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,14 @@
 //const designBouton = "";
 //export default designBouton;
 
+
+// import d'un module CSS
+import classes from '../css/style.css';
+
+export default () => {
+  console.log(classes.style);
+};
+
 //Pour que lorsque parcel se met à jour il ne garde pas les anciennes informations
 //mais remplace l'ancienne valeur par la nouvelle
 //j'inscris la méthode suivante
@@ -10,10 +18,3 @@ if (module.hot) {
     })
 }
 
-
-// import d'un module CSS
-import classes from './style.css';
-
-export default () => {
-  console.log(classes.style);
-};
--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -117,9 +117,121 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"js/script.js":[function(require,module,exports) {
+})({"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
 
-},{}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"css/style.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/main.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _style = _interopRequireDefault(require("../css/style.css"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+//const designBouton = "";
+//export default designBouton;
+// import d'un module CSS
+var _default = function _default() {
+  console.log(_style.default.style);
+}; //Pour que lorsque parcel se met à jour il ne garde pas les anciennes informations
+//mais remplace l'ancienne valeur par la nouvelle
+//j'inscris la méthode suivante
+
+
+exports.default = _default;
+
+if (module.hot) {
+  module.hot.accept(function () {
+    window.location.reload();
+  });
+}
+},{"../css/style.css":"css/style.css"}],"js/script.js":[function(require,module,exports) {
+"use strict";
+
+var _main = _interopRequireDefault(require("./main"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+//ici 
+//import nomDuModule './nomDuModule'
+//import designBouton './designBouton'
+//console.log(designBouton)
+//cet import export permet de lier les deux fichiers 
+(0, _main.default)();
+},{"./main":"js/main.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -147,7 +259,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56325" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63251" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -323,5 +435,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/script.js"], null)
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/script.js"], null)
 //# sourceMappingURL=/script.d573be0b.js.map
